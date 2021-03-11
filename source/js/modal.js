5513445aea1a84e4fb37e90f3d4bce6002cb5cf6
Drenyash <kavlod12345@gmail.com>
2021-03-12 01:38:39 +0300

    Страница профиля фрилансера

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,6 +1,7 @@
 const faqBtn = document.querySelector('.faq__btn');
 const modal = document.querySelector('.modal');
 const modalClose = modal.querySelector('.modal__cancel');
+const faqItem = document.querySelectorAll('.faq__element');
 
 faqBtn.addEventListener('click', ()=> {
   if (!modal.classList.contains('modal--active')) {
@@ -16,4 +17,11 @@ modalClose.addEventListener('click', ()=> {
   } else {
     modal.classList.remove('modal--active');
   }
+})
+
+faqItem.forEach((element)=> {
+  element.addEventListener('click', (evt)=> {
+    document.querySelectorAll(".faq__element").forEach(item => item.classList.remove("faq__element--active"));
+    element.classList.add("faq__element--active");
+  })
 })
