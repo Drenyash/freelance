a9e93404ec7aaf7fcf38862cd66d6ca84b8e8e3d
Drenyash <kavlod12345@gmail.com>
2021-03-18 13:28:50 +0300

    Основная вёрстка

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,19 @@
-const userNavigation = document.querySelector('.user__list');
-const userItem = userNavigation.querySelectorAll('.user__item');
+const avatar = document.querySelector('.user-nav__link--avatar');
+const userMenu = document.querySelector('.user__menu');
+const avatarElement = document.querySelectorAll('.user-nav__item');
 
-if (userNavigation.classList.contains('user__list--login')) {
-  userItem.forEach((element, i)=> {
-    if (!element.classList.contains('user__item--logout')) {
-      element.style.display = 'block';
-    }
-  })
-} else {
-  userItem.forEach((element, i)=> {
-    if (element.classList.contains('user__item--logout')) {
-      element.style.display = 'block';
-    }
-  })
-}
+avatar.addEventListener('click', (evt)=> {
+  evt.preventDefault();
+  if (!avatarElement[3].classList.contains('user-nav__item--active')) {
+    avatarElement[3].classList.add('user-nav__item--active');
+  } else {
+    avatarElement[3].classList.remove('user-nav__item--active');
+  }
+})
+
+document.addEventListener("click", (evt) => {
+  let target = evt.target;
+  if (!avatarElement[3].contains(target)) {
+    avatarElement[3].classList.remove('user-nav__item--active');
+  }
+})
